fb6c15f612d34e1b46a3a589849e550e07dca14d
unknown <matheusdamasceno15@hotmail.com>
2026-03-24 14:01:05 -0300

    conteudo do nosso cronometro

--- src/App.js
+++ src/App.js
@@ -1,14 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./estilo.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header >
-        <p>olá cronometro</p>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            numero: 0,
+            titleButton: 'VAI'
+        };
+        this.timer = null;
+        this.vai = this.vai.bind(this);
+        // this.pausar = this.pausar.bind(this);
+        this.limpar = this.limpar.bind(this);
+    }
+
+    vai() {
+        let state = this.state;
+
+        if (this.timer !== null) {
+            clearInterval(this.timer);
+            this.timer = null;
+            state.titleButton = "VAI"
+        } else {
+            this.timer = setInterval(() => {
+                let state = this.state;
+                state.numero += 0.1;
+                this.setState(state)
+            }, 100);
+            state.titleButton = "PAUSAR"
+        }
+
+        this.setState(state)
+    }
+
+    
+    limpar() {
+
+        if (this.timer !== null) {
+            clearInterval(this.timer);
+            this.timer = null;
+        } 
+
+        let state = this.state;
+        state.numero = 0;
+        state.titleButton = "VAI"
+        this.setState(state)
+    }
+
+
+    // forma extra de adicionar o botão pausar, adicionando mais conteudo a o nosso timer
+    // pausar() {
+    //     if (this.timer !== null) {
+    //         clearInterval(this.timer)
+    //         this.timer =  null;
+    //     }
+    // }
+
+
+
+    render() {
+        return (
+            <div className="container">
+                <h1 className="title">Bora cronometrar este treino!!</h1>
+                <img src={require('./assests/cronometro.png')} className="img" />
+                <a className="timer">{this.state.numero.toFixed(1)}</a>
+                <div className="areaBtn">
+                    <a className="botao" onClick={this.vai}>{this.state.titleButton}</a>
+                    {/* <a className="botao" onClick={this.pausar}>PAUSAR</a> */}
+                    <a className="botao" onClick={this.limpar}>LIMPAR</a>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default App;
